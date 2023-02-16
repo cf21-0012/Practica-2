151ea9f29457ab3d42ef17796633f2c1bdd2c009
cf21-0012 <104171322+cf21-0012@users.noreply.github.com>
2023-02-15 23:39:03 -0400

    Add files via upload

---
+++ Ejercicio 3 - Operaciones con cubos/Ejercicio 3 - Operaciones con cubos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3___Operaciones_con_cubos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa que calcula suma y resta de cubos.
+            Console.Write("Ingrese el primero: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int restaC = (int)(Math.Pow(n1, 3) - Math.Pow(n2, 3));
+            int sumaC = (int)(Math.Pow(n1, 3) + Math.Pow(n2, 3));
+           
+
+            Console.WriteLine("La suma de cubos de " +n1 +" y " +n2 +" es " + sumaC + " y la resta de cubos es " +restaC);
+            Console.Read();
+        }
+    }
+}
