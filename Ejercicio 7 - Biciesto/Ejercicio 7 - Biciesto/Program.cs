b4d691c3dbab9ba247f903ac0822cc8c91e2f186
cf21-0012 <104171322+cf21-0012@users.noreply.github.com>
2023-02-15 23:40:23 -0400

    Add files via upload

---
+++ Ejercicio 7 - Biciesto/Ejercicio 7 - Biciesto/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7___Biciesto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que determina si el año es bisiesto");
+            Console.WriteLine("Digite el año: ");
+            int año = int.Parse(Console.ReadLine());
+
+            if (Bisiesto(año))
+            {
+                Console.WriteLine(año + " es un año bisiesto.");
+            }
+            else
+            {
+                Console.WriteLine(año +" no es un año bisiesto.");
+            }
+
+            Console.ReadLine();
+        }
+
+        static bool Bisiesto(int a)
+        {
+            if (a % 4 != 0)
+            {
+                return false;
+            }
+            else if (a % 100 != 0)
+            {
+                return true;
+            }
+            else if (a % 400 != 0)
+            {
+                return false;
+            }
+            else if (a % 4000 != 0) 
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
