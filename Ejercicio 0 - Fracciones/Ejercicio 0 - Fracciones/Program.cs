e9cafbf79fcdf6c328049098afbbeb6e80c15f43
cf21-0012 <104171322+cf21-0012@users.noreply.github.com>
2023-02-15 23:37:56 -0400

    Add files via upload

---
+++ Ejercicio 0 - Fracciones/Ejercicio 0 - Fracciones/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_0___Fracciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que convierte fracciones mixtas a comunes");
+            Console.WriteLine("Introduzca el entero: ");
+            int e = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduzca el numerador: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduzca el denominador: ");
+            int d = int.Parse(Console.ReadLine());
+
+            e = e * d;
+            n = n + e;
+
+            Console.WriteLine("La fraccion resultante es: ");
+            Console.WriteLine(n);
+            Console.WriteLine("--");
+            Console.WriteLine(+d);
+
+
+        }
+    }
+}
