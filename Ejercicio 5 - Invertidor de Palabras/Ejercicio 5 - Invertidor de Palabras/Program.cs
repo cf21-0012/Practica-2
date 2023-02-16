d3f05a5d27163f9a8673509aafb20ddac20c4846
cf21-0012 <104171322+cf21-0012@users.noreply.github.com>
2023-02-15 23:39:38 -0400

    Add files via upload

---
+++ Ejercicio 5 - Invertidor de Palabras/Ejercicio 5 - Invertidor de Palabras/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5___Invertidor_de_Palabras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que imrpime las palabras invertidas");
+            Console.WriteLine("Ingrese una palabra: ");
+            string palabra = Console.ReadLine();
+
+            char[] letras = palabra.ToCharArray();
+            int length = letras.Length;
+            char[] letrasR = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                letrasR[i] = letras[length - i - 1];
+            }
+
+            string palabraR = new string(letrasR);
+
+            Console.WriteLine("La palabra invertida es: " +palabraR);
+
+            Console.ReadLine();
+        }
+    }
+}
